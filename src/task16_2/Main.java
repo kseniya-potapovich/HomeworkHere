6eda799c9a7669f787ad419a0f211fc34543b8bf
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-30 10:44:32 +0300

    Second commit

--- src/task16_2/Main.java
+++ src/task16_2/Main.java
@@ -10,15 +10,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<Character, Character> map = new HashMap<>();
-        List<String> strings  = new ArrayList<>();
+        List<String> strings = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             strings.add(scanner.nextLine());
         }
 
         for (int i = 0; i < 5; i++) {
-            map.put(strings.get(i).charAt(0), strings.get(i).charAt(strings.get(i).length()-1) );
+            map.put(strings.get(i).charAt(0), strings.get(i).charAt(strings.get(i).length() - 1));
         }
-
         System.out.println(map);
     }
 }
